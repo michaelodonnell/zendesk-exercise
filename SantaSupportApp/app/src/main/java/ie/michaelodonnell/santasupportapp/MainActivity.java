e8e97e47af40c2b3a855398fc945739a7ad7936c
Michael O'Donnell <mail@michaelodonnell.ie>
2019-12-27 19:22:27 +0000

    Add the Zendesk Request activity for messaging support

--- SantaSupportApp/app/src/main/java/ie/michaelodonnell/santasupportapp/MainActivity.java
+++ SantaSupportApp/app/src/main/java/ie/michaelodonnell/santasupportapp/MainActivity.java
@@ -1,5 +1,6 @@
 package ie.michaelodonnell.santasupportapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -14,12 +15,28 @@ import android.view.MenuItem;
 import android.view.View;
 
 import ie.michaelodonnell.santasupportapp.ui.main.SectionsPagerAdapter;
+import zendesk.core.AnonymousIdentity;
+import zendesk.core.Identity;
+import zendesk.core.Zendesk;
+import zendesk.support.Support;
+import zendesk.support.guide.HelpCenterActivity;
+import zendesk.support.request.RequestActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        /* Zendesk Start */
+        Zendesk.INSTANCE.init(this, "https://michaelodonnell.zendesk.com", "45c872ec411ce4d454a0fe69fdc12065acaad9ec839a27f0", "mobile_sdk_client_9ab55ce6c4a8c2379707");
+        Identity identity = new AnonymousIdentity();
+        Zendesk.INSTANCE.setIdentity(identity);
+        Support.INSTANCE.init(Zendesk.INSTANCE);
+        final Intent intent = RequestActivity.builder()
+                .intent(this);
+        /* Zendesk End */
+
         setContentView(R.layout.activity_main);
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
@@ -31,8 +48,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                startActivity(intent);
             }
         });
     }
